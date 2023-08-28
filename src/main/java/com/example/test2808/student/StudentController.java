b3d17f8a353f6132b4dca81687324288798c6f3c
haune77 <lyeminmyheart@gmail.com>
2023-08-28 09:22:41 +0700

    initializing spring boot project

---
+++ src/main/java/com/example/test2808/student/StudentController.java
@@ -0,0 +1,19 @@
+package com.example.test2808.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudent(){
+        return List.of(
+                "Nguyen Duong","Dan Bich"
+        );
+    }
+}
