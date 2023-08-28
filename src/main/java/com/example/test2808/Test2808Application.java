735db0f18a84089f81372a2eecfa3fc036f8f0da
haune77 <lyeminmyheart@gmail.com>
2023-08-28 09:22:41 +0700

    initializing spring boot project

---
+++ src/main/java/com/example/test2808/Test2808Application.java
@@ -0,0 +1,13 @@
+package com.example.test2808;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2808Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2808Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test2808/Test2808ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test2808;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2808ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
